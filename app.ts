fddd0254318743a5e37ff791c9ff1443121342d8
Halil <halil.seran.1135@gmail.com>
2022-09-02 21:24:45 +0300

    lastupdate

--- app.ts
+++ app.ts
@@ -2,3 +2,22 @@ const userName = "halil";
 console.log(userName);
 
 console.log("Time to get started...");
+
+let appId = 'abc';
+const button = document.querySelector('button')!;
+
+function add(n1: number, n2: number) {
+  if (n1 + n2 > 0) {
+    return n1 + n2;
+  }
+  return;
+}
+
+function clickHandler(message: string) {
+  // let userName = 'Max';
+  console.log('Clicked! ' + message);
+}
+// a comment
+if (button) {
+  button.addEventListener('click', clickHandler.bind(null, "You're welcome!"));
+}
